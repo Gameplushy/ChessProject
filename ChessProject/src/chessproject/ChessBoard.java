57fc8d39325513eaf963c66515b980b11bcf7552
Gameplushy <victor_f@orange.fr>
2021-11-15 22:26:22 +0100

    Movement Patterns For Every Piece

--- ChessProject/src/chessproject/ChessBoard.java
+++ ChessProject/src/chessproject/ChessBoard.java
@@ -36,14 +36,69 @@ public class ChessBoard {
     public ArrayList<int[]> checkPossibleMoves(Piece p, int[] coord){
         ArrayList<int[]> possibleMoves = new ArrayList<>();
         //Depends on type of piece
-        
+        switch(p.type){
+            case PAWN:
+                int[] newCoord = new int[]{coord[0]+(p.isWhite?-1:1),coord[1]};
+                if(isMovePossible(p.isWhite,newCoord)<=1) possibleMoves.add(newCoord);
+                break;
+            case BISHOP:
+                int[][] deltaListB = new int[][]{new int[]{-1,-1},new int[]{1,1},new int[]{-1,1},new int[]{1,-1}};
+                for(int[] delta : deltaListB){
+                    checkMovesInLine(p.isWhite,coord,delta,possibleMoves);
+                }
+                break;
+            case ROOK:
+                int[][] deltaListR = new int[][]{new int[]{-1,0},new int[]{1,0},new int[]{0,1},new int[]{0,-1}};
+                for(int[] delta : deltaListR){
+                    checkMovesInLine(p.isWhite,coord,delta,possibleMoves);
+                }
+                break;
+            case KNIGHT:
+                int[][] deltaListN = new int[][]{new int[]{-1,-2},new int[]{1,2},new int[]{-1,2},new int[]{1,-2},new int[]{-2,1},new int[]{2,1},new int[]{-2,-1},new int[]{2,-1}};
+                for(int[] delta : deltaListN){
+                    int[] testedCoord = new int[] {coord[0]+delta[0],coord[1]+delta[1]};
+                    if(isMovePossible(p.isWhite,testedCoord)<=1) possibleMoves.add(testedCoord);
+                }
+                break;
+            case KING:
+                int[][] deltaListK = new int[][]{new int[]{-1,-1},new int[]{1,1},new int[]{-1,1},new int[]{1,-1},new int[]{-1,0},new int[]{1,0},new int[]{0,1},new int[]{0,-1}};
+                for(int[] delta : deltaListK){
+                    int[] testedCoord = new int[] {coord[0]+delta[0],coord[1]+delta[1]};
+                    if(isMovePossible(p.isWhite,testedCoord)<=1) possibleMoves.add(testedCoord);
+                }
+                break;
+            case QUEEN:
+                int[][] deltaListQ = new int[][]{new int[]{-1,-1},new int[]{1,1},new int[]{-1,1},new int[]{1,-1},new int[]{-1,0},new int[]{1,0},new int[]{0,1},new int[]{0,-1}};
+                for(int[] delta : deltaListQ){
+                    checkMovesInLine(p.isWhite,coord,delta,possibleMoves);
+                }
+                break;
+            
+        };
         //for(int[] c : possibleMoves)
         //    if(!isMovePossible(p.isWhite,c)) possibleMoves.remove(c);       
         return possibleMoves;
     }
     
-    public boolean isMovePossible(boolean isAColor,int[] coord){
-        if(board[coord[0]][coord[1]]==null) return true;
-        else return isAColor!=board[coord[0]][coord[1]].isWhite;
+    private void checkMovesInLine(boolean color, int[] coord, int[] deltas, ArrayList<int[]> moveList){
+        int[] newCoord = new int[] {coord[0]+deltas[0],coord[1]+deltas[1]};
+        while(isMovePossible(color,newCoord)<=1){
+            moveList.add(newCoord.clone());
+            newCoord[0]+=deltas[0]; newCoord[1]+=deltas[1];
+        }
+    }
+    
+    /**
+    * Returns : 
+    * 0 : Empty cell
+    * 1 : Opposite piece
+    * 2 : Own piece
+    * 3 : Out of board
+    */
+    private int isMovePossible(boolean isAColor,int[] coord){
+        if(coord[0]<0||coord[0]>7||coord[1]<0||coord[1]>8) return 3;
+        if(board[coord[0]][coord[1]]==null) return 0;
+        if(isAColor!=board[coord[0]][coord[1]].isWhite) return 1;
+        return 2;
     }
 }
